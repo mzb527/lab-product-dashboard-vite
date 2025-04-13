1a8e01406759ce9e4da944e37d198b71dd0ffb6f
mzb527 <mzb527@gmail.com>
2025-04-12 20:38:14 -0400

    Lab Uploading

---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: "Product A", price: "$10", available: true },
+    { id: 2, name: "Product B", price: "$15", available: false },
+    { id: 3, name: "Product C", price: "$20", available: true },
+  ];
+  
+  export default products;
